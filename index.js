1362012fb0c953d1a250dc5f3eb11c0c93d8eaed
Pierre Donias <pierre.donias@gmail.com>
2017-02-14 14:23:45 +0100

    Simple implementation

--- index.js
+++ index.js
@@ -1,7 +1,30 @@
+const includes = require('lodash').includes
+
 const RE = /([0-9]{1,4},? )?((bis|ter|a|b|c),? )?(ch(emin)?|av(enue)?|rue|pl(ace)?|boulevard|bv?d|c(ou)?rs|all[ée]e)( [^\.,;: ]*){1,4}(( [^\.,;: ]*){0,3}[\.,;:])?/gi
+const FAKES = [ 'parking', 'calme', 'comprenant' ]
+
+function pure (address) {
+  return address.replace(/[.,]+/g, '').replace(/ +/g, ' ')
+}
+
+function checkFakes (address) {
+  for (fake of FAKES) {
+    if (includes(address, fake)) return false
+  }
+
+  return true
+}
 
 module.exports = function (text) {
-  const matches = RE.exec(text)
+  const addresses = []
+  let matches
 
-  return matches[0]
+  do {
+    matches = RE.exec(text)
+    if (matches && checkFakes(matches[0])) {
+      addresses.push(pure(matches[0]))
+    }
+  } while (matches)
+
+  return addresses
 }
--- test.js
+++ test.js
@@ -1,5 +1,25 @@
 const extract = require('./')
 
-test('test', () => {
-  expect(extract('6 rue Denfert Rochereau')).toBe('6 rue Denfert Rochereau')
+test('simple', () => {
+  expect(
+    extract('6 rue Denfert Rochereau')
+  ).toEqual([ '6 rue Denfert Rochereau' ])
+})
+
+test('double', () => {
+  expect(
+    extract('6 rue Denfert Rochereau, 41 rue du Drac')
+  ).toEqual([ '6 rue Denfert Rochereau', '41 rue du Drac' ])
+})
+
+test('real', () => {
+  expect(
+    extract('Petit appartement situé au 6, rue Denfert Rochereau, proche du bvd Clémenceau. Faibles charges.')
+  ).toEqual([ '6 rue Denfert Rochereau', 'bvd Clémenceau' ])
+})
+
+test('fake', () => {
+  expect(
+    extract('Petit appartement situé dans une rue calme, proche du bvd Clémenceau. Place de parking disponible.')
+  ).toEqual([ 'bvd Clémenceau' ])
 })
