b2823d043946525de612714a2f3d1845aec13d2a
Pierre Donias <pierre.donias@gmail.com>
2017-02-15 22:38:40 +0100

    Is it really better than master?

--- src/constants.js
+++ src/constants.js
@@ -1,45 +1,81 @@
+import {
+  filter,
+  map
+} from 'lodash'
+
 export const COMP = [
-  'bis',
-  'ter',
   'a',
   'b',
-  'c'
+  'bis',
+  'c',
+  'ter'
 ]
 
 export const TRIGGERS = [
-  'secteur',
+  'près',
   'proche',
-  'proximité'
+  'proximité',
+  'secteur'
 ]
 
-export const STREET_TRIGGERS = [
-  'chemin',
-  'avenue',
-  'av',
-  'rue',
-  'place',
-  'pl',
-  'boulevard',
-  'blvd',
-  'bvd',
-  'cours',
-  'crs',
+export const STREET_TYPES = [
   'allée',
-  'quai'
+  'avenue',
+  'boulevard',
+  'chemin',
+  'cours',
+  'place',
+  'quai',
+  'rue'
 ]
+
+export const ALIASES = {
+  'allée': [ 'allee' ],
+  avenue: [ 'av' ],
+  boulevard: [ 'bvd', 'blvd' ],
+  chemin: [ 'ch' ],
+  cours: [ 'crs' ],
+  place: [ 'pl' ],
+  'près': [ 'pres', 'prés' ],
+  'proximité': [ 'proximite' ]
+}
 
 export const FAKES = [
-  'parking',
-  'stationnement',
   'calme',
-  'comprenant'
+  'comprenant',
+  'parking',
+  'stationnement'
+]
+
+export const LINKERS = [
+  'de la',
+  'de',
+  'des',
+  'du'
 ]
 
 export const STOPS = [
-  '.',
+  ' à ',
   ',',
   ';',
   ':',
-  '(',
-  ')'
+  '\\.',
+  '\\(',
+  '\\)',
+  '\n',
+  '\r'
 ]
+
+export const SPLIT_RE = new RegExp(STOPS.join('|'), 'g')
+export const FAKES_RE = new RegExp(FAKES.join('|'), 'g')
+export const STREET_TYPES_PATTERN = STREET_TYPES.join('|')
+
+const _getAliases = words =>
+  map(filter(words, type => ALIASES[type]), type => ALIASES[type])
+
+const ALL_TRIGGERS = TRIGGERS
+  .concat(_getAliases(TRIGGERS))
+  .concat(STREET_TYPES)
+  .concat(_getAliases(STREET_TYPES))
+
+export const TRIGGER_RE = new RegExp(ALL_TRIGGERS.join('|'), 'g')
--- src/index.js
+++ src/index.js
@@ -1,7 +1,31 @@
 import * as _ from './constants'
+import {
+  forEach
+} from 'lodash'
 
-function extract (text) {
-  return ''
+function removeUselessSpaces (text) {
+  return text
+    .replace(/ +/g, ' ')
+    .replace(/^ | $/, '')
 }
 
-module.exports = extract
+module.exports = function (text) {
+  const addresses = []
+  const arr = text.split(_.SPLIT_RE)
+
+  forEach(arr, piece => {
+    if (_.FAKES_RE.exec(piece)) {
+      return
+    }
+
+    const matches = new RegExp(`([0-9]{1,4} *)?(${_.STREET_TYPES_PATTERN})(.*)`).exec(piece)
+
+    if (matches) {
+      addresses.push(removeUselessSpaces(
+        (matches[1] || '') + matches[2] + matches[3] // 6 + rue + Denfert Rochereau
+      ))
+    }
+  })
+
+  return addresses
+}
