73502e2acc74e352c77f2d86d8db497c6ba497bd
Pierre Donias <pierre.donias@gmail.com>
2017-02-18 16:51:47 +0100

    More fakes (#4)
    * à => [aà]
    
    * Add some more fakes

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 const includes = require('lodash').includes
 
 const RE = /([0-9]{1,4},? )?((bis|ter|a|b|c),? )?(ch(emin)?|av(enue)?|rue|pl(ace)?|boulevard|bv?d|c(ou)?rs|all[eé]e|quai|secteur|proche|proximit[eé])( [^.,;:() ]*){1,4}(( [^.,;:() ]*){0,3}[.,;:()])?/gi
-const FAKES = [ 'parking', 'stationnement', 'calme', 'comprenant' ]
+const FAKES = [ 'parking', 'stationnement', 'calme', 'agreable', 'agréable', 'recherché', 'comprenant', 'commerce', 'tram' ]
 
 function pure (address) {
   return address
     .replace(/[.,;:()]/g, '') // "6, rue foo" => "6 rue foo"
     .replace(/^ | $/g, '') // "6 rue foo " => "6 rue foo"
     .replace(/ +/g, ' ') // "6   rue  foo" => "6 rue foo"
-    .replace(/ à .*$/, '') // "6 rue foo à bar" => "6 rue foo"
+    .replace(/ [aà] .*$/, '') // "6 rue foo à bar" => "6 rue foo"
 }
 
 function checkFakes (address) {
